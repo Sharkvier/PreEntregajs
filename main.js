1e496b6e57d3d15d4c561e3d9d358f2d0a86fb7e
Shark <javier.sscschile@gmail.com>
2023-08-31 21:43:16 -0400

    entrega coder js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
-
 // Modificar título
 const titulo = document.getElementById("titulo");
 titulo.innerHTML = "<h1>Libreria Cosas Necesarias</h1> <h2>Buscar Libro</h2>";
+
 
 //constructor
 let iva = 1.19
@@ -17,6 +17,7 @@ class Libro {
         this.stock = stock;
     }
 }
+
 
 // Definición de los objetos 
 const libro1 = new Libro("it", "stephen king", "plaza janes", 1500, 100000, 5);
@@ -36,14 +37,14 @@ const libro12 = new Libro("el tao del jeet kune do","bruce lee","dojo",300,45000
 //array
 const libros =[libro1,libro2,libro3,libro4,libro5,libro6,libro7,libro8,libro9,libro10,libro11,libro12]
 
-// Función para mostrar resultados en el DOM y guardar en localStorage
+
+
+// Función para mostrar resultados en el DOM 
 function mostrarResultadosEnDOM(resultados) {
     const resultadoDiv = document.getElementById("resultados");
 
     // Operador ternario para decidir qué contenido asignar a resultadoDiv.innerHTML
     resultadoDiv.innerHTML = resultados.length > 0 ? 
-
-    // Si hay resultados el método map transforma cada objeto resultado en un fragmento de HTML
     resultados.map(({ nombre, autor, editorial, paginas, precio, precioConIva, stock }) =>
             `<div>
                 <p>Nombre: ${nombre}</p>
@@ -56,28 +57,100 @@ function mostrarResultadosEnDOM(resultados) {
                 <hr>
             </div>`
           )
-        : "<p>No se encontraron resultados.</p>";
-
+        .join(""): Swal.fire({
+            icon: "error",
+            title: "chanfle...",
+            text: "No se encontraron resultados."
+          });
+          return;
          // Los resultados los manda al localStorage
     if (resultados.length > 0) {
         localStorage.setItem("resultados", JSON.stringify(resultados));
     }
 }
 
-// Funciones para buscar libro por nombre o autor
+
+
+// Funcion para buscar libro por nombre 
 function buscarLibroPorNombre(event) {
     event.preventDefault();
-    const busqueda = document.getElementById("BuscaInputNombre").value.trim();
-    const resultados = libros.filter(({ nombre }) => nombre.includes(busqueda));
-    mostrarResultadosEnDOM(resultados);
+    const busqueda = document.getElementById("BuscaInputNombre").value.trim().toLowerCase();
+    
+    const resultados = libros.filter(({ nombre }) => nombre.toLowerCase().includes(busqueda));
+    
+    if (resultados.length > 0) {
+        mostrarResultadosEnDOM(resultados);
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "chanfle...",
+            text: "No se encontraron resultados."
+        });
+    }
 }
 
+
+// Funcion para buscar libro  por autor
 function buscarLibroPorAutor(event) {
     event.preventDefault();
-    const busqueda = document.getElementById("BuscaInputAutor").value.trim();
-    const resultados = libros.filter(({ autor }) => autor.includes(busqueda));
-    mostrarResultadosEnDOM(resultados);
+    const busqueda = document.getElementById("BuscaInputAutor").value.trim().toLowerCase();
+    const resultados = libros.filter(({ autor }) => autor.toLowerCase().includes(busqueda));
+    
+    if (resultados.length > 0) {
+        mostrarResultadosEnDOM(resultados);
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "chanfle...",
+            text: "No se encontraron resultados."
+        });
+    }
 }
+
+
+// Función para agregar un libro 
+function agregarLibro() {
+    const nombre = prompt("Ingrese el nombre del libro:");
+    const autor = prompt("Ingrese el autor del libro:");
+    const editorial = prompt("Ingrese la editorial del libro:");
+    const paginas = parseInt(prompt("Ingrese el número de páginas del libro:"));
+    const precio = parseInt(prompt("Ingrese el precio del libro:")); 
+    const stock = parseInt(prompt("Ingrese el stock del libro:"));
+
+    if(isNaN(precio)|| isNaN(paginas) || isNaN(stock) || nombre ==="" ||autor==="" || editorial===""){
+        Swal.fire({ 
+            icon: 'error',
+            title: 'Chanfle...',
+            text: 'Ingresa datos validos',
+          })
+        return;
+    }        
+
+    const nuevoLibro = new Libro(nombre, autor, editorial, paginas, precio, stock);
+
+    if (libros.some((liv) => liv.nombre === nuevoLibro.nombre)) {
+        Swal.fire({
+          icon: "warning",
+          title: "Advertencia",
+          text: " Ya hay Existencias"
+        });
+        
+      }
+
+    libros.push(nuevoLibro);
+    // Guardar los libros actualizados en el local storage
+    localStorage.setItem("libros", JSON.stringify(libros));
+    // Mostrar los resultados actualizados en el DOM
+    mostrarResultadosEnDOM(libros);
+    Swal.fire({
+        icon: 'success',
+        title: 'Listo',
+        text: 'Libro agregado!',   
+      })
+}
+
+
+
 
 // Eventos para mostrar resultados en el DOM
 const searchFormNombre = document.getElementById("FormularioNombre");
@@ -86,5 +159,71 @@ searchFormNombre.addEventListener("submit", buscarLibroPorNombre);
 const searchFormAutor = document.getElementById("FormularioAutor");
 searchFormAutor.addEventListener("submit", buscarLibroPorAutor);
 
+
+
+//Promesa
+const obtenerLibros = () => {
+    console.log("Obteniendo libros en stock");
+    return new Promise((resolve, reject) => {
+        if (!libros) {
+            reject("Error al obtener libros");
+        } else {
+            setTimeout(() => {
+                resolve(libros);
+            }, 2000);
+        }
+    });
+};
+
 const botonTotal = document.getElementById("total");
-botonTotal.addEventListener("click", () => mostrarResultadosEnDOM(libros));
+botonTotal.addEventListener("click", () => {
+    obtenerLibros()
+        .then((librosObtenidos) => {
+            localStorage.setItem("libros", JSON.stringify(librosObtenidos));
+            mostrarResultadosEnDOM(librosObtenidos);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+
+
+//Evento para agregar libros
+const botonAgregar = document.getElementById("agregar");
+botonAgregar.addEventListener("click", () => {
+    agregarLibro();
+    mostrarResultadosEnDOM(libros);
+});
+
+
+
+
+// mostrar proximos lanzamientos utilizando api desde un archivo json utilizando async y await
+const mostrarLanzamientosButton = document.getElementById("mostrar-lanzamientos");
+
+mostrarLanzamientosButton.addEventListener("click", async () => {
+    try {
+        const respuesta = await fetch('libros.json');
+        const data = await respuesta.json();
+        const nuevos = data.nuevos;
+
+        const nuevosContainer = document.getElementById("nuevos-container");
+
+        nuevos.forEach((x) => {
+            const nuevosLanzamientos = document.createElement("P");
+            nuevosLanzamientos.innerText = `Nombre: ${x.titulo},
+                                            Autor: ${x.autor},
+                                            Fecha:${x.fecha}`;
+            nuevosContainer.appendChild(nuevosLanzamientos);
+        });
+    } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title: "chanfle...",
+            text: "No se encontraron resultados."
+        });
+    }
+});
+
+ 
